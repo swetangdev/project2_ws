2148e6ae01c780a7988a712daad447988783a9ad
iamdevgeek <iamdevgeek@gmail.com>
2020-10-23 09:41:00 -0700

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/swetang/project2_ws/src
+# Build directory: /home/swetang/project2_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("my_robot")
+subdirs("robot_description")
---
+++ build/my_robot/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/swetang/project2_ws/src/my_robot
+# Build directory: /home/swetang/project2_ws/build/my_robot
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
